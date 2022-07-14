d5609458186e83e146c41e9753a89246c69862ca
Evgenii Boiko <jax7777@yandex.ru>
2022-07-14 23:06:35 +0000

    Added files for the 2nd Terraform homework

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+#Sleep fo 30 seconds
+echo 'Sleep fo 30 seconds: sleep 30'
+echo "$(sleep 30)"
+
 #Get key
 echo 'sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -'
 echo "$(sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -)"
 echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
+#Sleep fo 30 seconds
+echo 'Sleep fo 30 seconds: sleep 30'
+echo "$(sleep 30)"
+
 #Update system
-echo 'sudo apt update'
+echo 'sudo apt-get update'
 echo "$(sudo apt-get update)"
 
+#Sleep fo 50 seconds
+echo 'Sleep fo 50 seconds: sleep 50'
+echo "$(sleep 50)"
+
 #Install mongodb
-echo 'sudo apt install -y mongodb'
-echo "$(sudo apt install -y mongodb)"
+echo 'sudo apt-get install -y mongodb'
+echo "$(sudo apt-get install -y mongodb)"
+
+#Sleep fo 20 seconds
+echo 'Sleep fo 20 seconds: sleep 20'
+echo "$(sleep 20)"
 
 #Run service mongodb
 echo 'sudo systemctl start mongodb'
---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+
+  labels = {
+    tags = "reddit-app"
+  }
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.app_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
+
