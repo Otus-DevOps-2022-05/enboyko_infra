9486dce1aa8aeb22a7871f11aba3aa68f952003a
Evgenii Boiko <jax7777@yandex.ru>
2022-07-25 02:50:49 +0000

    Formally changed packer/key.json to pass tests

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -16,9 +16,6 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
-}
 variable subnet_id {
   description = "Subnet"
 }
