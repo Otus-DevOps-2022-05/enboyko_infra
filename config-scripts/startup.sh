46b9967f59ce4b19b6816ddee2fbe4e90ba69a51
Evgenii Boiko <jax7777@yandex.ru>
2022-07-25 02:03:48 +0000

    Added files for the 2nd Ansible homework

--- config-scripts/startup.sh
+++ config-scripts/startup.sh
@@ -17,3 +17,4 @@ yc compute instance create \
  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
  --metadata serial-port-enable=1 \
  --metadata-from-file user-data=config.yaml
+
